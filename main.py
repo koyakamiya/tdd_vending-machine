59bd9b20e61283b94339567423fdf0db1a661022
Koya Kamiya <koya_kamiya@albert2005.co.jp>
2021-04-08 21:14:21 +0900

    [add] add pre-commit and lock file

--- main.py
+++ main.py
@@ -3,5 +3,16 @@ from logging import config
 
 config.fileConfig("logging.conf", disable_existing_loggers=False)
 
+
+def greet_all(names: list[str]) -> None:
+    """Greet all.
+
+    Args:
+        names (list[str]): [description]
+    """
+    for name in names:
+        print("hello", name)
+
+
 if __name__ == "__main__":
     pass
