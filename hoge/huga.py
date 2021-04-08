dffe38dffc00dcc81cca0875e68c6e80373eaf4e
koyakamiya <32283503+koyakamiya@users.noreply.github.com>
2021-04-08 19:52:10 +0900

    Initial commit

---
+++ hoge/huga.py
@@ -0,0 +1,15 @@
+"""sample"""
+from logging import getLogger
+
+logger = getLogger(__name__)
+
+
+class Huga:
+    """
+    Huga class
+    """
+
+    def piyo(self) -> str:
+        """return piyo"""
+        logger.info("piyo")
+        return "piyo"
