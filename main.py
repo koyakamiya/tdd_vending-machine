39fc980e085c90c057c1f51f61203e644f56d683
Koya Kamiya <koya_kamiya@albert2005.co.jp>
2021-04-08 20:31:04 +0900

    [fix] remove old import

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 """main"""
 from logging import config
 
-from hoge.huga import Huga
 
 config.fileConfig("logging.conf", disable_existing_loggers=False)
 
 if __name__ == "__main__":
-    Huga().piyo()
+    pass
