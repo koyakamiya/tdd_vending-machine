6a2daa29f1fce46817c90d763773251f8524ca80
siida36 <siida0118@gmail.com>
2021-04-26 19:54:44 +0900

    [Remove] legacy tests

--- tests/vending_machine/test_money.py
+++ tests/vending_machine/test_money.py
@@ -3,14 +3,6 @@ from typing import Final
 from vending_machine.money import Money
 
 
-def test_cannot_create_2_yen():
-    assert "Y2" not in dir(Money)
-
-
-def test_cannot_create_0_yen():
-    assert "Y0" not in dir(Money)
-
-
 def test_create_japanese_valid_money():
     JAPANESE_MONEY_KIND: Final[list[str]] = [
         "Y1",
@@ -36,43 +28,3 @@ def test_create_japanese_valid_money():
 
     _test_money_kind()
     _test_money_value()
-
-
-def test_create_1_yen():
-    assert Money.Y1 == 1
-
-
-def test_create_5_yen():
-    assert Money.Y5 == 5
-
-
-def test_create_10_yen():
-    assert Money.Y10 == 10
-
-
-def test_create_50_yen():
-    assert Money.Y50 == 50
-
-
-def test_create_100_yen():
-    assert Money.Y100 == 100
-
-
-def test_create_500_yen():
-    assert Money.Y500 == 500
-
-
-def test_create_1000_yen():
-    assert Money.Y1000 == 1000
-
-
-def test_create_2000_yen():
-    assert Money.Y2000 == 2000
-
-
-def test_create_5000_yen():
-    assert Money.Y5000 == 5000
-
-
-def test_create_10000_yen():
-    assert Money.Y10000 == 10000
